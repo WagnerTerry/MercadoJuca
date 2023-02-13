8d6e98890bc801ee5b87620226d8f04ce6988ec2
Wagner Terry <wagner.goncalves@calindra.com.br>
2023-02-13 17:08:36 -0300

    criando primeiro state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import {useState} from 'react'
 import Cabecalho from './components/Cabecalho'
 
 function App() {
+  const [numero, setNumero] = useState()
+
+  function incrementar(){
+    console.log("funcao adicionar")
+    setNumero(numero + 1)
+  }
+
   return (
     <div className="home">
       <Cabecalho tema="home" />
       <br />
+      {numero}
       ------------------------------
       <Cabecalho tema="cadastro"/>
+      <button onClick={incrementar}>Adicionar</button>
+
     </div>
   )
 }
