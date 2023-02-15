8d9b996e9da19ebe415e56afedd42b4b344613cc
Migue da Silva Gomes <106116635+MiguelDaSilvaGomes@users.noreply.github.com>
2023-02-15 19:22:56 -0300

    modelo sass no Footer Home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,17 +8,16 @@ const Footer = () => {
       <p>Termos de Serviço</p>
       <p>Política de privacidade</p>
       <p>Cancelamento, troca e reembolso</p>
-    {/* ---------------------------------------------- */}
     <hr />
 
-    <div className='Inscrever'>
+    <div className='inscrever'>
     <p>Newsletter</p>
       <input placeholder='Digite seu melhor email' className='input'  type="text" />
       <button className='button' >Inscrever</button>
     </div>
     
-   <div id='fotos'>
-    <div id='Redes-Sociais'>
+   <div>
+    <div id='redes-Sociais'>
         <img  src='../src/assets/Instagram.png '/>
         <img  src='../src/assets/Facebook.png '/>
     </div>
