7f4ccec15109400ce6a513dbd0915fc45e641475
Migue da Silva Gomes <106116635+MiguelDaSilvaGomes@users.noreply.github.com>
2023-02-17 17:09:49 -0300

    Realocamento do input e do button no componente footer da home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,31 +4,43 @@ import './Footer.scss'
 const Footer = () => {
   return(
     <footer>
-      <p>Contato</p>
-      <p>Termos de Serviço</p>
-      <p>Política de privacidade</p>
-      <p>Cancelamento, troca e reembolso</p>
-    <hr />
+      <div className='links-de-servico'>
+          <div>
+          <p>Contato</p>
+          <p>Termos de Serviço</p></div>
+          
+          <p>Newslatter</p>
+        </div>
+        <div className='links-de-gerenciamento'>
+          <div>
+              <p>Política de privacidade</p>
+              <p>Cancelamento, troca e reembolso</p>
+          </div>
 
-    <div className='inscrever'>
-    <p>Newsletter</p>
-      <input placeholder='Digite seu melhor email' className='input'  type="text" />
-      <button className='button' >Inscrever</button>
-    </div>
-    
-   <div>
-    <div id='redes-Sociais'>
-        <img  src='../src/assets/Instagram.png '/>
-        <img  src='../src/assets/Facebook.png '/>
-    </div>
-     <div id='pagamentos'>
-     <img  src='../src/assets/MastercardM.png '/>
-       <img  src='../src/assets/PayPalP.png '/>
-       <img  src='../src/assets/Visa.png '/>
-     </div>
+          <div>
+              <input placeholder='Digite seu melhor email' className='input'  type="text" />
+              <button className='button' >Inscrever</button>
+          </div>
+      </div>
+          
+          <div className='inscrever'>
+              
+          </div>
+        <hr />
+        
+       <div>
+          <div id='redes-Sociais'>
+              <img  src='../src/assets/Instagram.png '/>
+              <img  src='../src/assets/Facebook.png '/>
+          </div>
+          <div id='pagamentos'>
+          <img  src='../src/assets/MastercardM.png '/>
+            <img  src='../src/assets/PayPalP.png '/>
+            <img  src='../src/assets/Visa.png '/>
+        </div>
+          
+      </div>
       
-   </div>
-  
     </footer>      
   )
 }
